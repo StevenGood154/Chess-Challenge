a67d8992cb05b855b072a58408ed7c14bfb9d742
Steven Good <steve.good.154@gmail.com>
2023-07-31 23:28:42 -0400

    - Trim eval function - add debug functions - sort moves based on capturing higher value piece

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -17,22 +17,47 @@ namespace ChessChallenge.Example
             this.move = move;
             this.node = node;
         }
+
         public int CompareTo(Edge? other)
         {
-            return other.node.moveStrength.CompareTo(this.node.moveStrength);
+            return other.node.moveStrength.CompareTo(node.moveStrength);
         }
     }
 
     public class Node
     {
-        public Edge? bestMove { get; set; } = null;
-        public int moveStrength { get; set; }
-        //public ulong? position { get; set; } = null;
-        public List<Edge>? edges { get; set; } = null;
-
         public Node(int moveStrength)
         {
             this.moveStrength = moveStrength;
+        }
+
+        public Edge? bestMove { get; set; }
+
+        public int moveStrength { get; set; }
+
+        //public ulong? position { get; set; } = null;
+        public List<Edge>? edges { get; set; }
+    }
+
+    public class TranspositionTable
+    {
+        private Dictionary<ulong, (int, int)> table = new Dictionary<ulong, (int, int)>();
+        public int? MoveStrength(ulong zobristKey, int depth)
+        {
+            if (table.ContainsKey(zobristKey))
+            {
+                var tuple = table[zobristKey];
+
+                if (depth <= tuple.Item1)
+                    return tuple.Item2;
+            }
+
+            return null;
+        }
+
+        public void LogBoard(ulong zobristKey, int depth, int score)
+        {
+            table[zobristKey] = (depth, score);
         }
     }
 
@@ -40,50 +65,60 @@ namespace ChessChallenge.Example
     {
         private readonly int[] _pieceValues = { 0, 100, 320, 300, 500, 900, 50000 };
 
-        private Node? _root = null;
+        private readonly int _bigNumber = 1000000000;
 
-        private bool _isWhite = false;
+        private bool _isWhite;
 
-        private int _bigNumber = Int32.MaxValue / 10;
+        private Node? _root;
+
+        private TranspositionTable tTable = new();
 
         public Move Think(Board board, Timer timer)
         {
-            //_root = null;
             if (_root == null)
             {
                 _isWhite = board.IsWhiteToMove;
-                _root = new Node(_isWhite ? Int32.MinValue : Int32.MaxValue);
+                _root = new Node(_isWhite ? -_bigNumber : _bigNumber);
             }
             else
             {
                 var lastMove = board.GameMoveHistory[^1];
                 var chosenEdge = _root.edges?.Where(edge => edge.move.Equals(lastMove))?.First();
                 if (chosenEdge != null)
-                {
                     _root = chosenEdge.node;
-                }
                 else
-                {
-                    _root = new Node(_isWhite ? Int32.MinValue : Int32.MaxValue);
-                }
+                    _root = new Node(_isWhite ? -_bigNumber : _bigNumber);
             }
 
-            var depth = 0;
+            var depthTimer = new List<int>();
 
-            while (timer.MillisecondsElapsedThisTurn < 500)
+            for (var depth = 3; ; depth++)
             {
-                if (depth >= 5)
-                    break;
-                Search(_root, depth, Int32.MinValue, Int32.MaxValue, board);
-                depth++;
+                if (depthTimer.Count >= 2 && depthTimer[depthTimer.Count - 2] > 0)
+                {
+                    var branchingFactor = depthTimer[depthTimer.Count - 1] / depthTimer[depthTimer.Count - 2];
+                    var nextLength = 2 * depthTimer[depthTimer.Count - 1] * branchingFactor;
+                    if (nextLength > 500)
+                    {
+                        //Console.WriteLine($"Evaluated Depth: {depth}");
+                        break;
+                    }
+                }
+                Search(_root, depth, -_bigNumber, _bigNumber, board);
+                depthTimer.Add(timer.MillisecondsElapsedThisTurn);
             }
+
+            //PrintBestLine(_root);
+            //Console.WriteLine();
+
             var ourMove = _root.bestMove;
             _root = ourMove.node;
             return ourMove.move;
         }
 
-        void Search(Node node, int depth, int alpha, int beta, Board board)
+        private void Search(Node node, int depth, int alpha, int beta, Board board)
         {
+            var tableResult = this.tTable.MoveStrength(board.ZobristKey, depth);
             if (depth == 0)
             {
                 node.moveStrength = EvaluatePosition(board);
@@ -92,7 +127,16 @@ namespace ChessChallenge.Example
 
             if (node.edges == null)
             {
-                node.edges = board.GetLegalMoves().Select(move => new Edge(move, new Node(!board.IsWhiteToMove ? -_bigNumber : _bigNumber))).ToList();
+                var legalMoves = board.GetLegalMoves();
+                Array.Sort(legalMoves, (Move a, Move b) =>
+                {
+                    var aVal = 0.5 * (Math.Abs(4.5 - a.StartSquare.Rank) + Math.Abs(4.5 - a.StartSquare.File)) + 0.5 * (Math.Abs(4.5 - a.TargetSquare.Rank) + Math.Abs(4.5 - a.TargetSquare.File)) - _pieceValues[(int)a.CapturePieceType];
+                    var bVal = 0.5 * (Math.Abs(4.5 - b.StartSquare.Rank) + Math.Abs(4.5 - b.StartSquare.File)) + 0.5 * (Math.Abs(4.5 - b.TargetSquare.Rank) + Math.Abs(4.5 - b.TargetSquare.File)) - _pieceValues[(int)b.CapturePieceType];
+
+                    return (int)(aVal - bVal);
+                });
+
+                node.edges = legalMoves.Select(move => new Edge(move, new Node(!board.IsWhiteToMove ? -_bigNumber : _bigNumber))).ToList();
             }
 
             if (node.edges.Count == 0)
@@ -101,12 +145,9 @@ namespace ChessChallenge.Example
                 return;
             }
 
-            node.moveStrength = board.IsWhiteToMove ? Int32.MinValue : Int32.MaxValue;
+            node.moveStrength = board.IsWhiteToMove ? -_bigNumber : _bigNumber;
 
-            if (!board.IsWhiteToMove)
-            {
-                node.edges.Reverse();
-            }
+            if (!board.IsWhiteToMove) node.edges.Reverse();
 
             foreach (var edge in node.edges)
             {
@@ -122,11 +163,10 @@ namespace ChessChallenge.Example
                         node.moveStrength = edge.node.moveStrength;
                         node.bestMove = edge;
                     }
+
                     alpha = Math.Max(alpha, node.moveStrength);
                     if (node.moveStrength >= beta)
-                    {
                         break;
-                    }
                 }
                 else
                 {
@@ -136,17 +176,17 @@ namespace ChessChallenge.Example
                         node.moveStrength = edge.node.moveStrength;
                         node.bestMove = edge;
                     }
+
                     beta = Math.Min(beta, node.moveStrength);
                     if (node.moveStrength <= alpha)
-                    {
                         break;
-                    }
                 }
 
                 //if (beta <= alpha)
                 //    break;
             }
 
+            tTable.LogBoard(board.ZobristKey, depth, node.moveStrength);
             node.edges.Sort();
             //node.moveStrength = board.IsWhiteToMove ? node.edges.First().node.moveStrength : node.edges.Last().node.moveStrength;
         }
@@ -168,18 +208,95 @@ namespace ChessChallenge.Example
 
                 foreach (var pieceList in allPieceLists)
                 {
-                    int pieceListValue = pieceList.Count * _pieceValues[(int)pieceList.TypeOfPieceInList];
+                    var listIsWhite = pieceList.IsWhitePieceList;
+                    var pieceListValue = pieceList.Count * _pieceValues[(int)pieceList.TypeOfPieceInList];
 
-                    if (!pieceList.IsWhitePieceList)
+                    switch (pieceList.TypeOfPieceInList)
+
                     {
-                        pieceListValue *= -1;
+                        case PieceType.Pawn:
+                            foreach (var pawn in pieceList)
+                            {
+                                var rank = pawn.Square.Rank;
+                                pieceListValue += listIsWhite
+                                    ? rank * rank
+                                    : (7 - rank) * (7 - rank);
+
+                                var file = pawn.Square.Rank;
+                                if (board.GetPiece(new Square(Math.Max(0, file - 1), rank - 1)).IsPawn ||
+                                    board.GetPiece(new Square(Math.Min(7, file + 1), rank - 1)).IsPawn)
+                                {
+                                    pieceListValue += 25;
+                                }
+                            }
+                            break;
+                        case PieceType.Knight or PieceType.Queen:
+                            foreach (var knight in pieceList)
+                            {
+                                var file = 2 * knight.Square.File - 7;
+                                var rank = 2 * knight.Square.Rank - 7;
+                                pieceListValue += 50 - 10 * (int)Math.Sqrt((rank * rank) + (file * file));
+                            }
+                            break;
+                        case PieceType.Bishop:
+                            foreach (var bishop in pieceList)
+                            {
+                                pieceListValue += 15 * (2 - Math.Min(Math.Abs(bishop.Square.Rank - bishop.Square.File), Math.Abs(bishop.Square.Rank + bishop.Square.File)));
+                            }
+                            break;
+                        case PieceType.Rook:
+                            foreach (var rook in pieceList)
+                            {
+                                var friendlyPawnBitboard = board.GetPieceBitboard(PieceType.Pawn, listIsWhite);
+                                var opponentPawnBitboard = board.GetPieceBitboard(PieceType.Pawn, !listIsWhite);
+
+                                var rookAttacksBitboard =
+                                    BitboardHelper.GetSliderAttacks(PieceType.Rook, rook.Square, board);
+
+                                for (int i = 0; i < 8; i++)
+                                {
+                                    BitboardHelper.ClearSquare(ref rookAttacksBitboard, new Square(i, rook.Square.Rank));
+                                }
+
+                                var attackedFriendlyPawnOnFileBitboard = friendlyPawnBitboard & rookAttacksBitboard;
+                                var attackedOpponentsPawnOnFileBitboard = opponentPawnBitboard & rookAttacksBitboard;
+
+                                if (attackedOpponentsPawnOnFileBitboard > 0)
+                                {
+                                    pieceListValue += 50;
+                                    if (attackedFriendlyPawnOnFileBitboard == 0)
+                                    {
+                                        pieceListValue += 40;
+                                    }
+                                }
+
+                            }
+                            break;
                     }
+
+                    if (!pieceList.IsWhitePieceList) pieceListValue *= -1;
 
                     evaluation += pieceListValue;
                 }
             }
 
             return evaluation;
+        }
+
+        void PrintBestLine(Node node)
+        {
+            Console.WriteLine($"Evaluated Strength: {node.moveStrength}");
+
+            Console.Write("Best Line: ");
+           
+            var bestMove = node.bestMove;
+            while (bestMove != null)
+            {
+                Console.Write(" ");
+                Console.Write(bestMove.move.ToString().Substring(7, 4));
+                bestMove = bestMove.node.bestMove;
+            }
+            Console.Write("\n");
         }
     }
 }
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -2,7 +2,6 @@ using ChessChallenge.API;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using ChessChallenge.Application;
 
 //using static ChessChallenge.Application.ConsoleHelper;
 
@@ -34,13 +33,12 @@ public class Node
 
     public int moveStrength { get; set; }
 
-    //public ulong? position { get; set; } = null;
     public List<Edge>? edges { get; set; }
 }
 
 public class TranspositionTable
 {
-    private Dictionary<ulong, (int, int)> table = new Dictionary<ulong, (int, int)>();
+    public Dictionary<ulong, (int, int)> table = new Dictionary<ulong, (int, int)>();
     public int? MoveStrength(ulong zobristKey, int depth)
     {
         if (table.ContainsKey(zobristKey))
@@ -72,6 +70,8 @@ public class MyBot : IChessBot
 
     private TranspositionTable tTable = new();
 
+    private Dictionary<int, (int, int)> depthTracker = new Dictionary<int, (int, int)>(); // #DEBUG
+
     public Move Think(Board board, Timer timer)
     {
         if (_root == null)
@@ -98,16 +98,34 @@ public class MyBot : IChessBot
                 var branchingFactor = depthTimer[depthTimer.Count - 1] / depthTimer[depthTimer.Count - 2];
                 var nextLength = 2 * depthTimer[depthTimer.Count - 1] * branchingFactor;
                 if (nextLength > 500)
-                    {
-                        //Console.WriteLine($"Evaluated Depth: {depth}");
-                        break;
-                    }
+                    break;
+                    
             }
             Search(_root, depth, -_bigNumber, _bigNumber, board);
             depthTimer.Add(timer.MillisecondsElapsedThisTurn);
         }
 
-        //PrintBestLine(_root);
+        var d = DebugGetDepth(_root);
+        if (!depthTracker.ContainsKey(d))
+        {
+            depthTracker[d] = (1, timer.MillisecondsElapsedThisTurn);
+        }
+        else
+        {
+            depthTracker[d] = (depthTracker[d].Item1 + 1, depthTracker[d].Item2 + timer.MillisecondsElapsedThisTurn);
+        }
+
+        Console.Write(timer.MillisecondsElapsedThisTurn);
+        Console.Write(" ms"); 
+        Console.WriteLine();
+        foreach (var key in depthTracker.Keys)
+        {
+            Console.Write(key);
+            Console.Write(": ");
+            Console.Write(depthTracker[key].Item2 / depthTracker[key].Item1);
+            Console.Write(" ms / move");
+            Console.WriteLine();
+        }
         Console.WriteLine();
 
         var ourMove = _root.bestMove;
@@ -117,7 +135,7 @@ public class MyBot : IChessBot
 
     private void Search(Node node, int depth, int alpha, int beta, Board board)
     {
-        var tableResult = this.tTable.MoveStrength(board.ZobristKey, depth);
+        //var tableResult = tTable.MoveStrength(board.ZobristKey, depth);
         if (depth == 0)
         {
             node.moveStrength = EvaluatePosition(board);
@@ -129,8 +147,12 @@ public class MyBot : IChessBot
             var legalMoves = board.GetLegalMoves();
             Array.Sort(legalMoves, (Move a, Move b) =>
             {
-                var aVal = 0.5 * (Math.Abs(4.5 - a.StartSquare.Rank) + Math.Abs(4.5 - a.StartSquare.File)) + 0.5 * (Math.Abs(4.5 - a.TargetSquare.Rank) + Math.Abs(4.5 - a.TargetSquare.File)) - _pieceValues[(int)a.CapturePieceType];
-                var bVal = 0.5 * (Math.Abs(4.5 - b.StartSquare.Rank) + Math.Abs(4.5 - b.StartSquare.File)) + 0.5 * (Math.Abs(4.5 - b.TargetSquare.Rank) + Math.Abs(4.5 - b.TargetSquare.File)) - _pieceValues[(int)b.CapturePieceType];
+                //var aVal = 0.5 * (Math.Abs(4.5 - a.StartSquare.Rank) + Math.Abs(4.5 - a.StartSquare.File)) + 0.5 * (Math.Abs(4.5 - a.TargetSquare.Rank) + Math.Abs(4.5 - a.TargetSquare.File)) - _pieceValues[(int)a.CapturePieceType];
+                //var bVal = 0.5 * (Math.Abs(4.5 - b.StartSquare.Rank) + Math.Abs(4.5 - b.StartSquare.File)) + 0.5 * (Math.Abs(4.5 - b.TargetSquare.Rank) + Math.Abs(4.5 - b.TargetSquare.File)) - _pieceValues[(int)b.CapturePieceType];
+
+                var aVal = _pieceValues[(int)a.CapturePieceType] - _pieceValues[(int)a.MovePieceType];
+                var bVal = _pieceValues[(int)b.CapturePieceType] - _pieceValues[(int)b.MovePieceType];
+
 
                 return (int)(aVal - bVal);
             });
@@ -146,7 +168,7 @@ public class MyBot : IChessBot
 
         node.moveStrength = board.IsWhiteToMove ? -_bigNumber : _bigNumber;
 
-        if (!board.IsWhiteToMove) node.edges.Reverse();
+        if (!board.IsWhiteToMove) node.edges.Reverse(); // slow
 
         foreach (var edge in node.edges)
         {
@@ -154,12 +176,13 @@ public class MyBot : IChessBot
             Search(edge.node, depth - 1, alpha, beta, board);
             board.UndoMove(edge.move);
 
+            var edgeStrength = edge.node.moveStrength;
             if (board.IsWhiteToMove)
             {
                 //node.moveStrength = Math.Max(node.moveStrength, edge.node.moveStrength);
-                if (edge.node.moveStrength > node.moveStrength)
+                if (edgeStrength > node.moveStrength)
                 {
-                    node.moveStrength = edge.node.moveStrength;
+                    node.moveStrength = edgeStrength;
                     node.bestMove = edge;
                 }
 
@@ -170,9 +193,9 @@ public class MyBot : IChessBot
             else
             {
                 //node.moveStrength = Math.Min(node.moveStrength, edge.node.moveStrength);
-                if (edge.node.moveStrength < node.moveStrength)
+                if (edgeStrength < node.moveStrength)
                 {
-                    node.moveStrength = edge.node.moveStrength;
+                    node.moveStrength = edgeStrength;
                     node.bestMove = edge;
                 }
 
@@ -185,116 +208,114 @@ public class MyBot : IChessBot
             //    break;
         }
 
-        tTable.LogBoard(board.ZobristKey, depth, node.moveStrength);
+        //tTable.LogBoard(board.ZobristKey, depth, node.moveStrength);
         node.edges.Sort();
         //node.moveStrength = board.IsWhiteToMove ? node.edges.First().node.moveStrength : node.edges.Last().node.moveStrength;
     }
 
     int EvaluatePosition(Board board)
     {
-        int evaluation = 0;
         if (board.IsInCheckmate())
         {
-            evaluation = board.IsWhiteToMove ? -_bigNumber : _bigNumber;
+            return board.IsWhiteToMove ? -_bigNumber : _bigNumber;
         }
-        else if (board.IsDraw())
-        {
-            evaluation = 0;
-        }
-        else
-        {
-            var allPieceLists = board.GetAllPieceLists();
 
-            foreach (var pieceList in allPieceLists)
+        if (board.IsDraw())
+        {
+            return 0;
+        }
+
+        var evaluation = 0;
+        var bitboard = board.AllPiecesBitboard;
+        while (bitboard > 0)
+        {
+            var index = BitboardHelper.ClearAndGetIndexOfLSB(ref bitboard);
+            var square = new Square(index);
+            var piece = board.GetPiece(square);
+
+            var rank = square.Rank;
+            var file = square.File;
+
+            var isWhite = piece.IsWhite;
+
+            var pieceValue = _pieceValues[(int)piece.PieceType];
+
+            switch (piece.PieceType)
             {
-                var listIsWhite = pieceList.IsWhitePieceList;
-                var pieceListValue = pieceList.Count * _pieceValues[(int)pieceList.TypeOfPieceInList];
+                case PieceType.Pawn:
+                    pieceValue += isWhite ? rank * rank : 49 - 2 * rank + rank * rank - 10;
 
-                switch (pieceList.TypeOfPieceInList)
+                    if (board.GetPiece(new Square(Math.Max(0, rank - 1), rank - 1)).IsPawn ||
+                        board.GetPiece(new Square(Math.Min(7, rank + 1), rank - 1)).IsPawn)
+                    {
+                        pieceValue += 15;
+                    }
+                    break;
+                case PieceType.Knight or PieceType.Queen:
+                    pieceValue += 50 - 10 * (int)Math.Sqrt((rank * rank) + (file * file));
+                    break;
+                case PieceType.Bishop:
+                    pieceValue += 15 * (2 - Math.Min(Math.Abs(rank - file), Math.Abs(rank + file)));
+                    break;
+                case PieceType.Rook:
+                    var friendlyPawnBitboard = board.GetPieceBitboard(PieceType.Pawn, isWhite);
+                    var opponentPawnBitboard = board.GetPieceBitboard(PieceType.Pawn, !isWhite);
 
-                {
-                    case PieceType.Pawn:
-                        foreach (var pawn in pieceList)
+                    var rookAttacksBitboard =
+                        BitboardHelper.GetSliderAttacks(PieceType.Rook, square, board);
+
+                    for (int i = 0; i < 8; i++)
+                    {
+                        BitboardHelper.ClearSquare(ref rookAttacksBitboard, new Square(i, rank));
+                    }
+
+                    var attackedFriendlyPawnOnFileBitboard = friendlyPawnBitboard & rookAttacksBitboard;
+                    var attackedOpponentsPawnOnFileBitboard = opponentPawnBitboard & rookAttacksBitboard;
+
+                    if (attackedOpponentsPawnOnFileBitboard > 0)
+                    {
+                        pieceValue += 50;
+                        if (attackedFriendlyPawnOnFileBitboard == 0)
                         {
-                            var rank = pawn.Square.Rank;
-                            pieceListValue += listIsWhite
-                                ? rank * rank
-                                : (7 - rank) * (7 - rank);
-
-                            var file = pawn.Square.Rank;
-                            if (board.GetPiece(new Square(Math.Max(0, file - 1), rank - 1)).IsPawn ||
-                                board.GetPiece(new Square(Math.Min(7, file + 1), rank - 1)).IsPawn)
-                            {
-                                pieceListValue += 25;
-                            }
+                            pieceValue += 40;
                         }
-                        break;
-                    case PieceType.Knight or PieceType.Queen:
-                        foreach (var knight in pieceList)
-                        {
-                            var file = 2 * knight.Square.File - 7;
-                            var rank = 2 * knight.Square.Rank - 7;
-                            pieceListValue += 50 - 10 * (int)Math.Sqrt((rank * rank) + (file * file));
-                        }
-                        break;
-                    case PieceType.Bishop:
-                        foreach (var bishop in pieceList)
-                        {
-                            pieceListValue += 15 * (2 - Math.Min(Math.Abs(bishop.Square.Rank - bishop.Square.File), Math.Abs(bishop.Square.Rank + bishop.Square.File)));
-                        }
-                        break;
-                    case PieceType.Rook:
-                        foreach (var rook in pieceList)
-                        {
-                            var friendlyPawnBitboard = board.GetPieceBitboard(PieceType.Pawn, listIsWhite);
-                            var opponentPawnBitboard = board.GetPieceBitboard(PieceType.Pawn, !listIsWhite);
+                    }
 
-                            var rookAttacksBitboard =
-                                BitboardHelper.GetSliderAttacks(PieceType.Rook, rook.Square, board);
-
-                            for (int i = 0; i < 8; i++)
-                            {
-                                BitboardHelper.ClearSquare(ref rookAttacksBitboard, new Square(i, rook.Square.Rank));
-                            }
-
-                            var attackedFriendlyPawnOnFileBitboard = friendlyPawnBitboard & rookAttacksBitboard;
-                            var attackedOpponentsPawnOnFileBitboard = opponentPawnBitboard & rookAttacksBitboard;
-
-                            if (attackedOpponentsPawnOnFileBitboard > 0)
-                            {
-                                pieceListValue += 50;
-                                if (attackedFriendlyPawnOnFileBitboard == 0)
-                                {
-                                    pieceListValue += 40;
-                                }
-                            }
-
-                        }
-                        break;
-                }
-
-                if (!pieceList.IsWhitePieceList) pieceListValue *= -1;
-
-                evaluation += pieceListValue;
+                    break;
+                default: // PieceType.King
+                    break;
             }
+            evaluation += isWhite ? pieceValue : -pieceValue;
         }
-
         return evaluation;
     }
 
-    //void PrintBestLine(Node node)
-    //{
-    //    Console.WriteLine($"Evaluated Strength: {node.moveStrength}");
+    void DebugPrintBestLine(Node node) // #DEBUG
+    { // #DEBUG
+        Console.WriteLine($"Evaluated Strength: {node.moveStrength}"); // #DEBUG
+  
+        Console.Write("Best Line: "); // #DEBUG
 
-    //    Console.Write("Best Line: ");
+        var bestMove = node.bestMove; // #DEBUG
+        while (bestMove != null) // #DEBUG
+        { // #DEBUG
+            Console.Write(" "); // #DEBUG
+            Console.Write(bestMove.move.ToString().Substring(7, 4)); // #DEBUG
+            bestMove = bestMove.node.bestMove;// #DEBUG
+        } // #DEBUG
+        Console.Write("\n"); // #DEBUG
+    } // #DEBUG
 
-    //    var bestMove = node.bestMove;
-    //    while (bestMove != null)
-    //    {
-    //        Console.Write(" ");
-    //        Console.Write(bestMove.move.ToString().Substring(7, 4));
-    //        bestMove = bestMove.node.bestMove;
-    //    }
-    //    Console.Write("\n");
-    //}
+    int DebugGetDepth(Node node) // #DEBUG
+    { // #DEBUG
+        var depth = 0; // #DEBUG
+        var bestMove = node.bestMove; // #DEBUG
+        while (bestMove != null) // #DEBUG
+        { // #DEBUG
+            depth++; // #DEBUG
+            bestMove = bestMove.node.bestMove;// #DEBUG
+        } // #DEBUG
+
+        return depth; //#DEBUG
+    } // #DEBUG
 }
