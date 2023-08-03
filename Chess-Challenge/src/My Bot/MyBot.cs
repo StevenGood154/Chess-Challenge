4f23d6a7b0570bb143c73a1a8be5f208a6db9166
Steven Good <steve.good.154@gmail.com>
2023-08-02 21:58:48 -0400

    See description
    - Add debug method to test the speed of 'Evaluate'
    - Fix multiple bugs in evaluation that cause white and black positions to be evaluated differently
    - remove Rook evaluation logic completely
    because it is token expensive and not super helpful
    - refactor search method to take board as the first argument

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,7 +1,12 @@
 using ChessChallenge.API;
+using Microsoft.CodeAnalysis.CSharp;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Security;
 
 //using static ChessChallenge.Application.ConsoleHelper;
 
@@ -52,6 +57,8 @@ public class MyBot : IChessBot
 
     public Move Think(Board board, Timer timer)
     {
+        Console.WriteLine($"Starting Evaluation: {EvaluatePosition(board)}"); // DEBUG
+
         if (_root == null)
         {
             _isWhite = board.IsWhiteToMove;
@@ -79,33 +86,33 @@ public class MyBot : IChessBot
                     break;
                     
             }
-            Search(_root, depth, -_bigNumber, _bigNumber, board);
+            Search(board, _root, depth, -_bigNumber, _bigNumber);
             depthTimer.Add(timer.MillisecondsElapsedThisTurn);
         }
 
         // ---------- START DEBUG -------------
-        var d = DebugGetDepth(_root);  // #DEBUG
-        if (!depthTracker.ContainsKey(d)) // #DEBUG
-        { // #DEBUG
-            depthTracker[d] = (1, timer.MillisecondsElapsedThisTurn); // #DEBUG
-        } // #DEBUG
-        else // #DEBUG
-        { // #DEBUG
-            depthTracker[d] = (depthTracker[d].Item1 + 1, depthTracker[d].Item2 + timer.MillisecondsElapsedThisTurn); // #DEBUG
-        } // #DEBUG
+        //var d = DebugGetDepth(_root);  // #DEBUG
+        //if (!depthTracker.ContainsKey(d)) // #DEBUG
+        //{ // #DEBUG
+        //    depthTracker[d] = (1, timer.MillisecondsElapsedThisTurn); // #DEBUG
+        //} // #DEBUG
+        //else // #DEBUG
+        //{ // #DEBUG
+        //    depthTracker[d] = (depthTracker[d].Item1 + 1, depthTracker[d].Item2 + timer.MillisecondsElapsedThisTurn); // #DEBUG
+        //} // #DEBUG
 
-        Console.Write(timer.MillisecondsElapsedThisTurn); // #DEBUG
-        Console.Write(" ms");  // #DEBUG
-        Console.WriteLine(); // #DEBUG
-        foreach (var key in depthTracker.Keys) // #DEBUG
-        { // #DEBUG
-            Console.Write(key); // #DEBUG
-            Console.Write(": "); // #DEBUG
-            Console.Write(depthTracker[key].Item2 / depthTracker[key].Item1); // #DEBUG
-            Console.Write(" ms / move"); // #DEBUG
-            Console.WriteLine(); // #DEBUG
-        } // #DEBUG
-        Console.WriteLine(); // #DEBUG
+        //Console.Write(timer.MillisecondsElapsedThisTurn); // #DEBUG
+        //Console.Write(" ms");  // #DEBUG
+        //Console.WriteLine(); // #DEBUG
+        //foreach (var key in depthTracker.Keys) // #DEBUG
+        //{ // #DEBUG
+        //    Console.Write(key); // #DEBUG
+        //    Console.Write(": "); // #DEBUG
+        //    Console.Write(depthTracker[key].Item2 / depthTracker[key].Item1); // #DEBUG
+        //    Console.Write(" ms / move"); // #DEBUG
+        //    Console.WriteLine(); // #DEBUG
+        //} // #DEBUG
+        //Console.WriteLine(); // #DEBUG
         // ---------- END DEBUG -------------
 
         var ourMove = _root.bestMove;
@@ -113,7 +120,7 @@ public class MyBot : IChessBot
         return ourMove.move;
     }
 
-    private void Search(Node node, int depth, int alpha, int beta, Board board)
+    private void Search(Board board, Node node, int depth, int alpha, int beta)
     {
         var zobristKey = board.ZobristKey;
         if (depth == 0)
@@ -141,7 +148,6 @@ public class MyBot : IChessBot
                 //var aVal = _pieceValues[(int)a.CapturePieceType] - _pieceValues[(int)a.MovePieceType];
                 //var bVal = _pieceValues[(int)b.CapturePieceType] - _pieceValues[(int)b.MovePieceType];
 
-
                 return _pieceValues[(int)a.CapturePieceType] - _pieceValues[(int)a.MovePieceType] - _pieceValues[(int)b.CapturePieceType] + _pieceValues[(int)b.MovePieceType];
             });
 
@@ -163,7 +169,7 @@ public class MyBot : IChessBot
         foreach (var edge in node.edges)
         {
             board.MakeMove(edge.move);
-            Search(edge.node, depth - 1, alpha, beta, board);
+            Search(board, edge.node, depth - 1, alpha, beta);
             board.UndoMove(edge.move);
 
             var edgeStrength = edge.node.moveStrength;
@@ -232,44 +238,21 @@ public class MyBot : IChessBot
             switch (piece.PieceType)
             {
                 case PieceType.Pawn:
-                    pieceValue += isWhite ? rank * rank : 49 - 2 * rank + rank * rank - 10;
-
-                    if (board.GetPiece(new Square(Math.Max(0, file - 1), rank - 1)).IsPawn ||
-                        board.GetPiece(new Square(Math.Min(7, file + 1), rank - 1)).IsPawn)
+                    pieceValue += isWhite ? rank * rank : 49 - 14 * rank + rank * rank;
+                    var pawnDirection = isWhite ? 1 : -1;
+                    if (board.GetPiece(new Square(Math.Max(0, file - 1), rank - pawnDirection)).IsPawn ||
+                        board.GetPiece(new Square(Math.Min(7, file + 1), rank - pawnDirection)).IsPawn)
                     {
                         pieceValue += 15;
                     }
                     break;
                 case PieceType.Knight or PieceType.Queen:
-                    pieceValue += 50 - 10 * (int)Math.Sqrt((rank * rank) + (file * file));
+                    pieceValue += 50 - 10 * (int)Distance(3.5, 3.5, file, rank); // TODO: Rewrite to use distance function
                     break;
                 case PieceType.Bishop:
                     pieceValue += 15 * (2 - Math.Min(Math.Abs(rank - file), Math.Abs(rank + file)));
                     break;
                 case PieceType.Rook:
-                    var friendlyPawnBitboard = board.GetPieceBitboard(PieceType.Pawn, isWhite);
-                    var opponentPawnBitboard = board.GetPieceBitboard(PieceType.Pawn, !isWhite);
-
-                    var rookAttacksBitboard =
-                        BitboardHelper.GetSliderAttacks(PieceType.Rook, square, board);
-
-                    for (int i = 0; i < 8; i++)
-                    {
-                        BitboardHelper.ClearSquare(ref rookAttacksBitboard, new Square(i, rank));
-                    }
-
-                    var attackedFriendlyPawnOnFileBitboard = friendlyPawnBitboard & rookAttacksBitboard;
-                    var attackedOpponentsPawnOnFileBitboard = opponentPawnBitboard & rookAttacksBitboard;
-
-                    if (attackedOpponentsPawnOnFileBitboard == 0)
-                    {
-                        pieceValue += 50;
-                    }
-                    if (attackedFriendlyPawnOnFileBitboard == 0)
-                    {
-                        pieceValue += 20;
-                    }
-
                     break;
                 default: // PieceType.King
                     break;
@@ -279,8 +262,23 @@ public class MyBot : IChessBot
         return evaluation;
     }
 
+    double Distance(double x1, double y1, int x2, int y2) // TODO: Investigate static
+    {
+        return Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+    }
 
     // ---------- START DEBUG -------------
+
+    void DebugRunEvaluateSpeedTest(Board board, int numberOfRuns) // #DEBUG
+    { // #DEBUG
+        Stopwatch sw = Stopwatch.StartNew(); // #DEBUG
+        for (int i = 0; i < numberOfRuns; i++) {  // #DEBUG
+            EvaluatePosition(board); // #DEBUG
+        } // #DEBUG
+        sw.Stop(); // #DEBUG
+        Console.WriteLine($"'Evaluate' ran {numberOfRuns} times in {(double)sw.ElapsedMilliseconds / 1000} s"); // #DEBUG
+    } // #DEBUG
+
     void DebugPrintBestLine(Node node) // #DEBUG
     { // #DEBUG
         Console.WriteLine($"Evaluated Strength: {node.moveStrength}"); // #DEBUG
